9a8bedf99a0a810b2b0c057dee02d00fff5e698d
Ashish Prakash Singh <ashish977194@gmail.com>
2024-01-09 03:24:57 +0530

    2. Add Two Numbers

---
+++ 2. Add New Number/code.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* temp = new ListNode(0);
+        ListNode* curr = temp;
+        int carry = 0;
+        // int sum = 0 ;
+
+        while(l1 != NULL || l2 != NULL || carry == 1){
+            int sum = 0;
+            if(l1 != NULL){
+                sum+=l1->val;
+                l1 = l1->next;
+            }
+            if(l2 != NULL){
+                sum+=l2->val;
+                l2 = l2->next;
+            }
+            sum +=carry;
+            carry = sum/10;
+            ListNode* node = new ListNode(sum%10);
+            curr -> next = node;
+            curr = curr -> next;
+        }
+        return temp -> next;
+    }
+};
